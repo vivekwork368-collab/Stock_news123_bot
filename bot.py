dd2a15fd62f81e94049f89b4ff2163851ee32937
vivekwork368-collab <vivek.work368@gmail.com>
2026-02-26 12:56:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def main():
     app = Application.builder().token(TOKEN).build()  # new API
     app.add_handler(CommandHandler("start", start))
     print("Bot running...")
-    await app.run_polling()
+    app.run_polling(drop_pending_updates=true)
 
 if __name__ == "__main__":
     asyncio.run(main())
