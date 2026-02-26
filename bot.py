c488d1618d1694634de766f367d14ab771fedde2
vivekwork368-collab <vivek.work368@gmail.com>
2026-02-26 11:27:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,8 +41,7 @@ class DummyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Bot is running 🚀")
-
+        self.wfile.write("Bot is running 🚀".encode("utf-8"))
 def run_dummy_server():
     port = int(os.environ.get("PORT", 10000))  # Render assigns PORT automatically
     server = HTTPServer(("0.0.0.0", port), DummyHandler)
